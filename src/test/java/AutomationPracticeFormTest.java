e2150457f207a765843a7e26d66897aa6ee99158
TellSamm <chernikov.semen21@gmail.com>
2023-02-20 17:46:00 +0300

    added test demo version-2

--- src/test/java/AutomationPracticeFormTest.java
+++ src/test/java/AutomationPracticeFormTest.java
@@ -1,3 +1,6 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -8,21 +11,40 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AutomationPracticeFormTest {
 
+    @BeforeAll
+    static void openPracticeForm(){
+        Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com/automation-practice-form");
+        System.out.println("Start Test!");
+    }
+
     @Test
     void FillingForm(){
-        open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue("Semen");
         $("#lastName").setValue("Chernikov");
         $("#userEmail").setValue("chernikov.semen21@gmail.com");
         $("#gender-radio-1").sendKeys(" ");
         $("#userNumber").setValue("9196742969");
-        //$("#dateOfBirthInput").setValue("26 May 1990");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker-wrapper").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        $(".react-datepicker__year-select").selectOptionByValue("1990");
+        $("div.react-datepicker__day[aria-label='Choose Saturday, May 26th, 1990']").click();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbies-checkbox-1").setValue(" ");
+        $("label[for='hobbies-checkbox-1']").click();
+        $("label[for='hobbies-checkbox-2']").click();
+        $("label[for='hobbies-checkbox-3']").click();
         $("input.form-control-file").uploadFile(new File("src/test/resources/photo_2022-12-06_17-04-37.jpg"));
-        $("#currentAddress").setValue("429620, Чувашия. Чувашская Республика, р-н. Красноармейский, с. Красноармейское, ул. Новая, д. 6");
-        $("#state").selectOptionByValue("NCR");
-        $("#city").selectOptionByValue("Delhi");
+        $("#currentAddress").setValue("Chuvashuya, Cheboksary, boulvar Mittova d.3");
+        $("div#state div.css-tlfecz-indicatorContainer").click();
+        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
         $("#submit").click();
+
+
     }
+
+
 }
