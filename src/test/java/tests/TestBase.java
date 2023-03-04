507926e087b6a07859bffedae19b0de82dd3c068
TellSamm <chernikov.semen21@gmail.com>
2023-03-05 01:04:21 +0300

    deleted unused dependencies

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
